4bcb9952ebac50c46657359c0c706531ff2be39e
Anastasia <evgenfom@gmail.com>
2023-07-18 12:01:59 +0300

    git_ignore

--- Sem01/Program.cs
+++ Sem01/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите число 1: ");
+// int numberOne = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2: ");
+// int numberTwo = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+//  if (numberOne > numberTwo)
+//  {
+//     max = numberOne;
+
+//  } else {
+//     max = numberTwo;
+//  }
+//  Console.WriteLine(max);
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите число 1: ");
+// int numberOne = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2: ");
+// int numberTwo = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 3: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+//  if (numberOne > numberTwo)
+//  {
+//     max = numberOne;
+
+//  } else {
+//     max = numberTwo;
+//  } if(num3>max){
+//     max = num3;
+//  }
+//  Console.WriteLine(max);
+
+//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите число : ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num % 2 ==0){
+//     Console.WriteLine("Число четное");
+// }else{
+//     Console.WriteLine("Число нечетное");
+// }
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i =1; i <= num; i++)
+{
+    if(i % 2 ==0)
+    Console.WriteLine(i + "");
+}
