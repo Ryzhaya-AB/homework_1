640cd678ad5da7103ecd33c1f580533f00165694
Anastasia <evgenfom@gmail.com>
2023-07-18 17:17:51 +0300

    homework_2

--- Sem01/Program.cs
+++ Sem01/Program.cs
@@ -57,11 +57,13 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-for(int i =1; i <= num; i++)
-{
-    if(i % 2 ==0)
-    Console.WriteLine(i + "");
-}
+// for(int i =1; i <= num; i++)
+// {
+//     if(i % 2 ==0)
+//     Console.WriteLine(i + "");
+// }
+
+
