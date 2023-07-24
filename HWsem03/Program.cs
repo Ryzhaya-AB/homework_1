ab30ffab73b3986e4cbbbe8939a442ef9ee5646b
Anastasia <evgenfom@gmail.com>
2023-07-24 22:25:01 +0300

    sem3

--- HWsem03/Program.cs
+++ HWsem03/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+// Console.Write("Введите x1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z1: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите x2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z2: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+// Console.Write($"Расстояние между точками {Math.Round(result, 2)}" );
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.Write("Введите N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= n; i++)
+// {
+//     Console.WriteLine($"{Math.Pow(i, 3)}");
+// }
+
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = num1 ;
+int result = 0;
+while(num1 > 0)
+{
+    int var = num1 % 10;
+    res = res * 10 + d;
+    num1 = num1/10;
+}
+if(num2 == res)
+{
+    Console.Write("Число является полидромом");
+
+}else Console.Write("Число не является полидромом");
