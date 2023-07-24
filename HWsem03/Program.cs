258b43be9d588482a5e01f3711deda0c49a0d6fd
Anastasia <evgenfom@gmail.com>
2023-07-24 22:27:45 +0300

    change

--- HWsem03/Program.cs
+++ HWsem03/Program.cs
@@ -5,33 +5,34 @@
 
 Console.Clear();
 
-// Console.Write("Введите x1: ");
-// int x1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите y1: ");
-// int y1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите z1: ");
-// int z1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите x2: ");
-// int x2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите y2: ");
-// int y2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите z2: ");
-// int z2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-// double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-// Console.Write($"Расстояние между точками {Math.Round(result, 2)}" );
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.Write($"Расстояние между точками {Math.Round(result, 2)}" );
 
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// задача 23 
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-// Console.Write("Введите N: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// for (int i = 1; i <= n; i++)
-// {
-//     Console.WriteLine($"{Math.Pow(i, 3)}");
-// }
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{Math.Pow(i, 3)}");
+}
 
 
 // Задача 19
